01b4301ee40bbdf1ba343aff5ce9a53cbe7ddce9
Ubuntu <ubuntu@ip-10-254-153-60.us-west-2.compute.internal>
2014-05-31 04:25:27 +0000

    api module init files

--- functional/cs_problems/python/api/server.py
+++ functional/cs_problems/python/api/server.py
@@ -3,8 +3,8 @@ from flask import request
 
 app = Flask(__name__)
 
-from most_consecutive_item import lib as most_consecutive_item
-from most_frequent_item import lib as most_frequent_item
+from most_consecutive_item import lib as mci
+from most_frequent_item import lib as mfi
 
 def is_int(s):
     try: 
@@ -20,14 +20,14 @@ def to_int_array(arr):
 @app.route("/most_consecutive/<arr>")
 def most_consecutive_item(arr):
     arr_ints = to_int_array(arr)
-    res = most_consecutive_item.findMostConsecutivelyRepeatingValue_ideal(arr_ints)
+    res = mci.findMostConsecutivelyRepeatingValue_ideal(arr_ints)
     return 'Input Array = {}<br>Value with the most consecutive appearances = {}<br>Number of consecutive appearances = {}'\
         .format(arr_ints, res[0] if res else 'None', res[1] if res else 'None')
 
 @app.route("/most_frequent/<arr>")
 def most_frequent_item(arr):
     arr_ints = to_int_array(arr)
-    res = most_frequent_itme.findMostFrequentValue_ideal(arr_ints)
+    res = mfi.findMostFrequentValue_ideal(arr_ints)
     return 'Input Array = {}<br>Most frequent value = {}<br>Number of appearances = {}'\
         .format(arr_ints, res[0] if res else 'None', res[1] if res else 'None')
 
